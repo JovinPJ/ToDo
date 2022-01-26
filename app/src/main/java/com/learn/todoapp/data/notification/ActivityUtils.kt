75070a1ddb7a19ad687f46faa8c7b4fd9d9ef061
Jovin <jovinpj@gmail.com>
2022-01-26 17:26:31 +0530

    * playing Alarm sound on FUll Screen.
    Signed-off-by: jovinpj <jovinpj@gmail.com>

--- app/src/main/java/com/learn/todoapp/data/notification/ActivityUtils.kt
+++ app/src/main/java/com/learn/todoapp/data/notification/ActivityUtils.kt
@@ -3,6 +3,9 @@ package com.learn.todoapp.data.notification
 import android.app.Activity
 import android.app.KeyguardManager
 import android.content.Context
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Build
 import android.view.WindowManager
 
@@ -34,4 +37,26 @@ fun Activity.turnScreenOffAndKeyguardOn() {
                     or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
         )
     }
+}
+
+fun Activity.playAlarm(): Ringtone? {
+    val alert: Uri? = getAlarm()
+    var ringtone: Ringtone? = null
+    alert?.let {
+        ringtone = RingtoneManager.getRingtone(applicationContext, alert)
+        ringtone?.play()
+    }
+    return ringtone
+}
+
+fun getAlarm(): Uri? {
+    var alert: Uri? = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+
+    if (alert == null) {
+        alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        if (alert == null) {
+            alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+        }
+    }
+    return alert
 }
